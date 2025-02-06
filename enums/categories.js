52afff39e566280fca5a09be76f87a61bf0fa6a3
prageethmilan <prageethmilan1999@gmail.com>
2025-02-06 17:32:21 +0530

    Create Categories and Districts enums

---
+++ enums/categories.js
@@ -0,0 +1,16 @@
+const Categories = Object.freeze({
+    VENUE_LOCATION: "Venue & Location",
+    CATERING_FOOD_SERVICES: "Catering & Food Services",
+    DECORATIONS_RENTALS: "Decorations & Rentals",
+    ENTERTAINMENT_PERFORMERS: "Entertainment & Performers",
+    PHOTOGRAPHY_VIDEOGRAPHY: "Photography & Videography",
+    EVENT_PLANNING_COORDINATION: "Event Planning & Coordination",
+    INVITATIONS_PRINTING: "Invitations & Printing Services",
+    TRANSPORTATION: "Transportation",
+    BEAUTY_FASHION: "Beauty & Fashion",
+    SOUND_LIGHTING: "Sound & Lighting",
+    RELIGIOUS_CULTURAL: "Religious & Cultural Services",
+    GIFTS_FAVORS: "Gifts & Favors"
+});
+
+module.exports = Categories
---
+++ enums/districts.js
@@ -0,0 +1,29 @@
+const Districts = Object.freeze({
+    AMPARA: "Ampara",
+    ANURADHAPURA: "Anuradhapura",
+    BADULLA: "Badulla",
+    BATTICALOA: "Batticaloa",
+    COLOMBO: "Colombo",
+    GALLE: "Galle",
+    GAMPAHA: "Gampaha",
+    HAMBANTOTA: "Hambantota",
+    JAFFNA: "Jaffna",
+    KALUTARA: "Kalutara",
+    KANDY: "Kandy",
+    KEGALLE: "Kegalle",
+    KILINOCHCHI: "Kilinochchi",
+    KURUNEGALA: "Kurunegala",
+    MANNAR: "Mannar",
+    MATALE: "Matale",
+    MATARA: "Matara",
+    MONARAGALA: "Monaragala",
+    MULLAITIVU: "Mullaitivu",
+    NUWARA_ELIYA: "Nuwara Eliya",
+    POLONNARUWA: "Polonnaruwa",
+    PUTTALAM: "Puttalam",
+    RATNAPURA: "Ratnapura",
+    TRINCOMALEE: "Trincomalee",
+    VAVUNIYA: "Vavuniya"
+});
+
+module.exports = Districts
