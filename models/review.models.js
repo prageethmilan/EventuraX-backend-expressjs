4f8fa53a679eafb8ac95ba363043797c820f6c7b
prageethmilan <prageethmilan1999@gmail.com>
2025-03-05 20:00:39 +0530

    Updated

--- models/review.models.js
+++ models/review.models.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const reviewSchema = new mongoose.Scheme({
+const reviewSchema = new mongoose.Schema({
     userName: {
         type: String,
         required: true
